13264383040ecc6477b1f898f69b896538c8711e
GrandTheBest <shadowgamer11@mail.ru>
2024-11-04 08:58:15 +0500

    Messenger fix v2

--- TeleSploit.py
+++ TeleSploit.py
@@ -14,6 +14,7 @@ wh="\033[1;37m"
 ye="\033[1;33m"
 
 toolname = "TeleSploit"
+version = "v1.6 Beta 2"
 inpt = "telesploit > "
 
 options = ["Test connection", "Scrap members(Group)", "Scrap members(Channel)", "Send message", "Scrap dialogs", "Scrap messsages", "Scrap photos", "Scrap videos", "RealTime Messenger"]
@@ -40,7 +41,8 @@ def banner():
     print(re)
     tprint(toolname)
     print(wh)
-    print("TeleSploit - a utility for advanced interaction with Telegram\n")
+    print("TeleSploit - a utility for advanced interaction with Telegram")
+    print(f"{cy}Version: {version}{wh}\n")
     print("Select option \n")
 
     i = 0
@@ -446,41 +448,40 @@ async def main():
                 else:
                     _target = await client.get_entity(int(target))
 
-                async for message in client.iter_messages(target):
-                    if "@" in target:
-                        async for message in client.iter_messages(target, reverse=True):
-                            date = message.date.strftime("%Y-%m-%d %H:%M:%S")
-                            is_reply = ""
+                if "@" in target:
+                    async for message in client.iter_messages(target, reverse=True):
+                        date = message.date.strftime("%Y-%m-%d %H:%M:%S")
+                        is_reply = ""
 
-                            if message.is_reply:
-                                is_reply = f"{gr}Is reply!{wh}"
-                            else:
-                                is_reply = f"{re}Not reply!{wh}"
+                        if message.is_reply:
+                            is_reply = f"{gr}Is reply!{wh}"
+                        else:
+                            is_reply = f"{re}Not reply!{wh}"
+                        try:
+                            from_user = await client.get_entity(message.from_id.user_id)
+                            ID = message.from_id.user_id
+                            print(f"{cy}@{from_user.username}({ye}{ID}{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
+                        except AttributeError:
                             try:
-                                from_user = await client.get_entity(message.from_id.user_id)
-                                ID = message.from_id.user_id
+                                from_user = await client.get_entity(message.peer_id.user_id)
+                                ID = message.peer_id.user_id
                                 print(f"{cy}@{from_user.username}({ye}{ID}{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
                             except AttributeError:
-                                try:
-                                    from_user = await client.get_entity(message.peer_id.user_id)
-                                    ID = message.peer_id.user_id
-                                    print(f"{cy}@{from_user.username}({ye}{ID}{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
-                                except AttributeError:
-                                    print(f"{cy}@{re}None{cy}({re}None{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
-                    else:
-                        async for message in client.iter_messages(int(target), reverse=True):
-                            date = message.date.strftime("%Y-%m-%d %H:%M:%S")
+                                print(f"{cy}@{re}None{cy}({re}None{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
+                else:
+                    async for message in client.iter_messages(int(target), reverse=True):
+                        date = message.date.strftime("%Y-%m-%d %H:%M:%S")
+                        try:
+                            from_user = await client.get_entity(message.from_id.user_id)
+                            ID = message.from_id.user_id
+                            print(f"{cy}@{from_user.username}({ye}{ID}{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
+                        except AttributeError:
                             try:
-                                from_user = await client.get_entity(message.from_id.user_id)
-                                ID = message.from_id.user_id
+                                from_user = await client.get_entity(message.peer_id.user_id)
+                                ID = message.peer_id.user_id
                                 print(f"{cy}@{from_user.username}({ye}{ID}{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
                             except AttributeError:
-                                try:
-                                    from_user = await client.get_entity(message.peer_id.user_id)
-                                    ID = message.peer_id.user_id
-                                    print(f"{cy}@{from_user.username}({ye}{ID}{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
-                                except AttributeError:
-                                    print(f"{cy}@{re}None{cy}({re}None{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
+                                print(f"{cy}@{re}None{cy}({re}None{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
                 while True:
                     msg = input(wh+"\ntelesploit("+re+"RealTimeMessenger/"+target+wh+") > ")
                     await client.send_message(_target, msg)
