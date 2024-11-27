67bb91e2414424b4494d3f5ef6fb96bcb0512bbb
GR么ND <77930427+GrandTheBest@users.noreply.github.com>
2024-11-27 07:50:17 +0500

    Release v1.5.4 Added delete messages

--- TeleSploit.py
+++ TeleSploit.py
@@ -17,7 +17,7 @@ toolname = "TeleSploit"
 version = "v1.6 Beta 2"
 inpt = "telesploit > "
 
-options = ["Test connection", "Scrap members(Group)", "Scrap members(Channel)", "Send message", "Scrap dialogs", "Scrap messsages", "Scrap photos", "Scrap videos", "RealTime Messenger", "Delete chat"]
+options = ["Test connection", "Scrap members(Group)", "Scrap members(Channel)", "Send message", "Scrap dialogs", "Scrap messsages", "Scrap photos", "Scrap videos", "RealTime Messenger", "Delete chat", "Delete message"]
 
 for_replace = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]
 
@@ -58,6 +58,7 @@ async def main():
     global groups
     global channels
     global chats
+    global _messages
 
     while True:
         chats = []
@@ -509,6 +510,61 @@ async def main():
                     await client.delete_dialog(chats[int(dialog)].id, revoke=False)
 
                 print(f"\n{gr}Dialog successfuly removed!{wh}\n")
+            elif int(s) == 10:
+                banner()
+
+                _messages.clear()
+                chats.clear()
+
+                async for chat in client.iter_dialogs():
+                    chats.append(chat)
+
+                i = 0
+                for chat in chats:
+                    print(f"{gr}[{cy}{i}{gr}] {chat.title}{wh}")
+                    i += 1
+                
+                print(f"\n{wh}Choose the dialog.")
+                dialog = input(wh+"\ntelesploit("+re+"deleteMessage/"+wh+") > ")
+                
+                async for message in client.iter_messages(chats[int(dialog)], reverse=True):
+                    _messages.append(message)
+
+                i = 0
+                for message in _messages:
+                    date = message.date.strftime("%Y-%m-%d %H:%M:%S")
+                    is_reply = ""
+
+                    if message.is_reply:
+                        is_reply = f"{gr}Is reply!{wh}"
+                    else:
+                        is_reply = f"{re}Not reply!{wh}"
+                    try:
+                        from_user = await client.get_entity(message.from_id.user_id)
+                        ID = message.from_id.user_id
+                        print(f"{gr}[{cy}{i}{gr}] {cy}@{from_user.username}({ye}{ID}{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
+                    except AttributeError:
+                        try:
+                            from_user = await client.get_entity(message.peer_id.user_id)
+                            ID = message.peer_id.user_id
+                            print(f"{gr}[{cy}{i}{gr}] {cy}@{from_user.username}({ye}{ID}{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
+                        except AttributeError:
+                            print(f"{gr}[{cy}{i}{gr}] {cy}@{re}None{cy}({re}None{cy}): {gr}{message.text}{wh}. {date}. {is_reply}")
+                    i += 1
+                
+                print(f"\n{wh}Choose the message.")
+
+                message = input(wh+"\ntelesploit("+re+"deleteMessage/"+wh+") > ")
+
+                print("\nRemove for everyone? Yes or No. Default: No")
+                isRevoke = input(wh+"\ntelesploit("+re+"deleteMessage/removeForEveryone"+wh+") > ")
+
+                if isRevoke.lower() == "yes":
+                    await client.delete_messages(chats[int(dialog)].id, _messages[int(message)].id, revoke=True)
+                else:
+                    await client.delete_messages(chats[int(dialog)].id, _messages[int(message)].id, revoke=False)
+
+                print(f"\n{gr}Message successfuly removed!{wh}\n")
             else:
                 banner()
                 print("\nInvalid option\n")
